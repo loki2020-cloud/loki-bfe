db7da18a61b900d71963ab1d099a354f5c915fc0
Kamlesh R <k.ratrns@gmail.com>
2021-06-17 17:28:12 +0530

    TEST123

--- scripts/const.py
+++ scripts/const.py
@@ -1,6 +1,6 @@
 BFE_HOST = "batfish.nexariacloud.com"
 BFE_PORT = 443
-NETWORK_NAME = "test_net"
+NETWORK_NAME = "THOR"
 INIT_SNAPSHOT_NAME="baseline"
 INIT_SNAPSHOT_DIR="../reference_bgp"
 SLACK_CHANNEL = 'netops_mntc'
